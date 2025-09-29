a44976cea982b3c5c29427bb3bf937e3af2577df
alyssa.daigle <alyssa.daigle@unh.edu>
2025-09-29 13:13:57 -0400

    fixed formatting and theming

--- Expt1/ELISA/Expt1_ELISA_May2025.R
+++ Expt1/ELISA/Expt1_ELISA_May2025.R
@@ -1,19 +1,28 @@
 library(MCMCglmm)
 library(dplyr)
-library(tidyverse)
+library(tidyr)
 library(ggplot2)
 library(cowplot)
 library(viridis)
+library(ggtext)
 
-setwd(
-    "~/Library/CloudStorage/OneDrive-UniversityofNewHampshire/GreenManureProject/Expt1/expt1_dataanalysis/ELISA"
-)
+# --------------------------
+# Themes and variance function
+# --------------------------
+source("thesis_theme.R") # TPTN_theme() now includes cyanobacteria colors
+source("variance_explained.R") # ssbyvar()
+source("config_paths.R") # data_folder path
+plots_folder <- "plots"
 
-ELISA_dat <- read.csv("/Users/alyssadaigle/Desktop/csvs/Expt1_Phase2_ELISA.csv")
-
-ELISA_dat <- ELISA_dat |> select(-c(X, X.1, X.2, empty.tube.weight))
-
-ELISA_dat <- ELISA_dat |>
+# --------------------------
+# Load data
+# --------------------------
+ELISA_dat <- read.csv(file.path(
+    data_folder,
+    "ELISA",
+    "Expt1_Phase2_ELISA.csv"
+)) %>%
+    select(-c(X, X.1, X.2, empty.tube.weight)) %>%
     separate(
         treatment,
         into = c("geno", "cyano", "micro"),
@@ -21,56 +30,57 @@ ELISA_dat <- ELISA_dat |>
         remove = FALSE
     )
 
-#removing outliers
+# Remove outliers
 ELISA_dat_filtered <- ELISA_dat %>%
     filter(!is.na(cyano)) %>%
     filter(
-        !(sample == "25_c2" & treatment == "M_N_N") &
-            !(sample == "26_b3" & treatment == "W_Y_N")
+        !(sample == "25_c2" & treatment == "M_N_N"),
+        !(sample == "26_b3" & treatment == "W_Y_N")
     )
 
-#linear modeling
+# --------------------------
+# GLMM
+# --------------------------
 ELISA_glmm <- MCMCglmm(
     MC ~ -1 + cyano:micro,
     data = ELISA_dat_filtered,
-    verbose = F,
+    verbose = FALSE,
     nitt = 101000,
     thin = 10,
     burnin = 1000
 )
 summary(ELISA_glmm)
 
-
+# Posterior means and 95% CI
 post_summ <- summary(ELISA_glmm)$solutions
 ci_df <- as.data.frame(post_summ)
 ci_df$Effect <- rownames(ci_df)
-names(ci_df)[c(1, 2, 3)] <- c("PostMean", "Lower95CI", "Upper95CI")
+names(ci_df)[1:3] <- c("PostMean", "Lower95CI", "Upper95CI")
 
 ggplot(ci_df, aes(x = Effect, y = PostMean)) +
     geom_point(size = 3) +
     geom_errorbar(aes(ymin = Lower95CI, ymax = Upper95CI), width = 0.2) +
-    theme_minimal() +
     labs(
         title = "Posterior Means and 95% Credible Intervals",
         y = "Posterior Mean ± 95% CI",
         x = "Effect"
     ) +
+    TPTN_theme() +
     theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
-
-#plotting cyanoY vs cyanoN for MC concentration
-ELISA_dat_filtered$micro <- factor(
-    ELISA_dat_filtered$micro,
-    levels = c("N", "H", "KF", "ODR")
-)
+# --------------------------
+# Plot MC by microbiome source
+# --------------------------
+ELISA_dat_filtered <- ELISA_dat_filtered %>%
+    mutate(micro = factor(micro, levels = c("N", "H", "KF", "ODR")))
 
 MCplot <- ggplot(ELISA_dat_filtered, aes(x = micro, y = MC, color = cyano)) +
     stat_summary(
         fun = mean,
         geom = "point",
-        shape = 16,
         size = 4,
-        position = position_dodge(width = .5)
+        shape = 16,
+        position = position_dodge(0.5)
     ) +
     stat_summary(
         fun.data = mean_sdl,
@@ -78,9 +88,8 @@ MCplot <- ggplot(ELISA_dat_filtered, aes(x = micro, y = MC, color = cyano)) +
         geom = "errorbar",
         width = 0.2,
         size = 1,
-        position = position_dodge(width = .5)
+        position = position_dodge(0.5)
     ) +
-    scale_color_manual(values = c("N" = "black", "Y" = "aquamarine4")) +
     scale_x_discrete(
         labels = c(
             "N" = "Uninoculated",
@@ -89,83 +98,45 @@ MCplot <- ggplot(ELISA_dat_filtered, aes(x = micro, y = MC, color = cyano)) +
             "ODR" = "Dairy Farm"
         )
     ) +
-    labs(x = "Microbiome Source", y = "Microcystin (µg/g Duckweed)") +
-    theme_cowplot() +
-    theme(
-        legend.position = "none",
-        axis.title.x = ggtext::element_markdown(size = 9),
-        axis.title.y = element_text(size = 9),
-        axis.text.x = element_text(size = 8),
-        axis.text.y = element_text(size = 8)
-    )
+    labs(
+        x = "Microbiome Source",
+        y = "Microcystin (µg/g Duckweed)",
+        color = "Cyanobacteria Treatment"
+    ) +
+    TPTN_theme() +
+    theme(legend.position = "none")
 
 MCplot
 
-#sums of squares function
-ssbyvar <- function(response, category.vec) {
-    means <- tapply(response, category.vec, mean, na.rm = T)
-    ssresid <- sum(sapply(sort(unique(category.vec)), function(z) {
-        sum(
-            (response[category.vec == z] - means[names(means) == z])^2,
-            na.rm = T
-        )
-    }))
-    sstot <- sum((response - mean(response, na.rm = T))^2, na.rm = T)
-    sst <- (sstot - ssresid)
-    return(sst / sstot)
-}
-
-variance_cyano <- ssbyvar(ELISA_dat_filtered$MC, ELISA_dat_filtered$cyano)
-variance_micro <- ssbyvar(ELISA_dat_filtered$MC, ELISA_dat_filtered$micro)
-variance_geno <- ssbyvar(ELISA_dat_filtered$MC, ELISA_dat_filtered$geno)
-
-MC_variance_data <- data.frame(
+# --------------------------
+# Variance explained
+# --------------------------
+MC_variance_data <- tibble(
     Factor = c("Cyanobacteria", "Microbiome", "Genotype"),
-    Variance = c(variance_cyano, variance_micro, variance_geno)
+    Variance = c(
+        ssbyvar(ELISA_dat_filtered$MC, ELISA_dat_filtered$cyano),
+        ssbyvar(ELISA_dat_filtered$MC, ELISA_dat_filtered$micro),
+        ssbyvar(ELISA_dat_filtered$MC, ELISA_dat_filtered$geno)
+    )
 )
 
-#stacked bar plot
 MC_variance_plot <- ggplot(
     MC_variance_data,
     aes(x = "", y = Variance, fill = Factor)
 ) +
     geom_bar(stat = "identity", position = "stack") +
-    labs(x = "", y = "Variation Explained", fill = "Effect") +
-    scale_fill_manual(
-        values = c(
-            "Cyanobacteria" = "aquamarine4",
-            "Genotype" = "darkblue",
-            "Microbiome" = "wheat"
-        )
-    ) +
-    guides(
-        fill = guide_legend(
-            title.position = "top",
-            title.hjust = .5,
-            override.aes = list(size = 1)
-        )
-    ) +
+    labs(y = "Variation Explained") +
     scale_y_continuous(
-        limits = c(0, 1), # Set the limits between 0 and 1
+        limits = c(0, 1),
         expand = expansion(mult = c(0, 0.05))
     ) +
-    theme_cowplot() +
-    theme(
-        legend.position = "right",
-        legend.justification = "center",
-        legend.box.just = "center",
-        legend.title = element_text(size = 7),
-        legend.text = element_text(size = 6),
-        legend.key.size = unit(0.2, "cm"),
-        legend.box = "vertical",
-        legend.box.spacing = unit(0, "cm"),
-        axis.text = element_text(size = 6),
-        axis.title = element_text(size = 7),
-        axis.ticks.x = element_blank(),
-    )
+    TPvariance_theme()
 
 MC_variance_plot
 
+# --------------------------
+# Combined plot
+# --------------------------
 MC_combined_plot <- plot_grid(
     MCplot,
     MC_variance_plot,
@@ -175,34 +146,22 @@ MC_combined_plot <- plot_grid(
 
 MC_combined_plot
 
-ggsave2(
-    "Expt1_MC_plot.jpg",
+ggsave(
+    file.path(plots_folder, "Expt1_MC_plot.jpg"),
     plot = MC_combined_plot,
     width = 6,
     height = 3.5,
     dpi = 500
 )
-ggsave(
-    "~/Library/CloudStorage/OneDrive-UniversityofNewHampshire/GreenManureProject/WRITING/plots/Expt1_MC_plot.jpg",
-    MC_combined_plot,
-    width = 6,
-    height = 3.5,
-    dpi = 500
-)
 
-
-#Line plot, MC by genotype
-MC_line_plot <- ELISA_dat |>
-    drop_na() |>
-    group_by(cyano, geno) |>
-    summarise(
-        mean_conc = mean(MC),
-        sd_conc = sd(MC),
-        .groups = 'drop'
-    ) |>
-    mutate(
-        se_conc = sd_conc / sqrt(n()) # Calculate standard error
-    ) |>
+# --------------------------
+# Line plot by genotype
+# --------------------------
+MC_line_plot <- ELISA_dat %>%
+    drop_na() %>%
+    group_by(cyano, geno) %>%
+    summarise(mean_conc = mean(MC), sd_conc = sd(MC), .groups = 'drop') %>%
+    mutate(se_conc = sd_conc / sqrt(n())) %>%
     ggplot(aes(
         x = cyano,
         y = mean_conc,
@@ -210,49 +169,45 @@ MC_line_plot <- ELISA_dat |>
         color = geno,
         group = geno
     )) +
-    scale_x_discrete(expand = c(0, 0.1), labels = c("N" = "No", "Y" = "Yes")) +
     geom_path(linewidth = 1, lineend = "round") +
     geom_errorbar(
         aes(ymin = mean_conc - se_conc, ymax = mean_conc + se_conc),
         width = 0.05,
-        size = .75
+        size = 0.75
     ) +
+    scale_x_discrete(expand = c(0, 0.1), labels = c("N" = "No", "Y" = "Yes")) +
     labs(
         x = "*M. aeruginosa* Spike",
         y = "Average Microcystin (µg/g Duckweed)",
         color = "Duckweed\nGenotype"
     ) +
-    guides(color = guide_legend(ncol = 1)) +
-    theme_cowplot() +
+    scale_color_viridis(discrete = TRUE) +
     theme(
         legend.position = "right",
         axis.title.x = ggtext::element_markdown(size = 9),
         axis.title.y = element_text(size = 9),
-        axis.text.x = element_text(size = 8),
-        axis.text.y = element_text(size = 8),
+        axis.text = element_text(size = 8),
         legend.title = element_text(size = 8),
         legend.text = element_text(size = 7)
-    ) +
-    scale_color_viridis(discrete = TRUE)
+    )
 
-# Display the line plot
 MC_line_plot
 
 ggsave(
-    "Expt1_MC_line_plot.jpg",
+    file.path(plots_folder, "Expt1_MC_line_plot.jpg"),
     plot = MC_line_plot,
     width = 4,
     height = 3,
     dpi = 300
 )
 
-
-##splitting it up by micro
-
+# --------------------------
+# Split by microbiome
+# --------------------------
 micro_mod <- MCMCglmm(
     MC ~ -1 + micro:cyano,
     data = ELISA_dat_filtered,
-    verbose = F,
+    verbose = FALSE,
     nitt = 101000,
     thin = 10,
     burnin = 1000
@@ -266,20 +221,16 @@ micro_labels <- c(
     "KF" = "Kingman \nFarm"
 )
 
-ELISA_dat_filtered <- ELISA_dat_filtered |>
-    mutate(micro = factor(micro, levels = c("N", "H", "KF", "ODR")))
-
 MCplot_micro <- ggplot(
     ELISA_dat_filtered,
     aes(x = micro, y = MC, color = cyano)
 ) +
-    #geom_jitter(shape = 1, width = 0.2, size = 2) +
     stat_summary(
         fun = mean,
         geom = "point",
-        shape = 16,
         size = 4,
-        position = position_dodge(width = 0.6)
+        shape = 16,
+        position = position_dodge(0.6)
     ) +
     stat_summary(
         fun.data = mean_sdl,
@@ -287,36 +238,28 @@ MCplot_micro <- ggplot(
         geom = "errorbar",
         width = 0.3,
         size = 1,
-        position = position_dodge(width = 0.6)
+        position = position_dodge(0.6)
     ) +
-    scale_color_manual(
-        values = c("N" = "black", "Y" = "aquamarine4"),
-        labels = c("N" = "No", "Y" = "Yes")
-    ) +
+    scale_color_manual(labels = c("N" = "No", "Y" = "Yes")) +
+    scale_x_discrete(labels = micro_labels) +
     labs(
         x = "Microbiome Source",
-        color = "*M. aeruginosa* <br> Spike",
-        y = "Microcystin (µg/g Duckweed)"
+        y = "Microcystin (µg/g Duckweed)",
+        color = "*M. aeruginosa* <br> Spike"
     ) +
-    scale_x_discrete(labels = micro_labels) +
-    theme_cowplot() +
+    TPTN_theme() +
     theme(
         legend.position = "right",
         legend.title = ggtext::element_markdown(size = 9),
-        legend.text = element_text(size = 8),
-        axis.title.y = element_text(size = 9),
-        axis.text.x = element_text(size = 8),
-        axis.text.y = element_text(size = 8),
-        axis.title.x = element_text(size = 9)
+        legend.text = element_text(size = 8)
     )
-
 
 MCplot_micro
 
-ggsave(
-    "Expt1_MC_micro_plot.jpg",
-    plot = MCplot_micro,
-    width = 5,
-    height = 3,
-    dpi = 500
-)
+#ggsave(
+#    file.path(plots_folder, "Expt1_MC_micro_plot.jpg"),
+#    plot = MCplot_micro,
+#    width = 5,
+#    height = 3,
+#   dpi = 500
+#)
