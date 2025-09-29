7046dcf75245f807652cd9a8d6ec71bb8cb9c933
alyssa.daigle <alyssa.daigle@unh.edu>
2025-09-29 12:36:39 -0400

    added config files

--- Expt1/Total-Phosphorus/Expt1_TPanalysis.R
+++ Expt1/Total-Phosphorus/Expt1_TPanalysis.R
@@ -6,17 +6,20 @@ library(cowplot)
 library(tibble)
 
 # --------------------------
-#themes and variance function
+# themes and variance function
 # --------------------------
-source("thesis_theme.R") # contains TPTN_theme() and TPvariance_theme()
-source("variance_explained.R") # contains ssbyvar()
+source("thesis_theme.R") # TPTN_theme() and TPvariance_theme()
+source("variance_explained.R") # ssbyvar()
+source("config_paths.R") # hidden paths
 
 # --------------------------
-#data
+# data
 # --------------------------
-TP_dat <- read.csv(
-    "/Users/alyssadaigle/Desktop/GM-Thesis-DataAnalysis/Expt1/Total-Phosphorus/Expt1_TP.csv"
-) |>
+TP_dat <- read.csv(file.path(
+    data_folder,
+    "Total-Phosphorus",
+    "Expt1_TP.csv"
+)) |>
     separate(
         treatment,
         into = c("geno", "cyano", "micro"),
@@ -40,12 +43,12 @@ micro_labels <- c(
 )
 
 # --------------------------
-#normality check
+# normality check
 # --------------------------
 shapiro.test(TP_dat$ppb)
 
 # --------------------------
-#GLMMs
+# GLMMs
 # --------------------------
 # Home microbiome
 tp_microH <- filter(TP_dat, micro == "H")
@@ -92,7 +95,7 @@ ggplot(ci_df, aes(x = Effect, y = PostMean)) +
     theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
 # --------------------------
-#plots
+# Plots
 # --------------------------
 # 1. Home Microbiome
 TPplot_microH <- ggplot(tp_microH, aes(x = geno, y = ppb, color = cyano)) +
@@ -157,10 +160,7 @@ TPplot_others <- ggplot(
         size = 0.5,
         position = position_dodge(width = 0.6)
     ) +
-    scale_color_manual(
-        values = c("N" = "black", "Y" = "aquamarine4"),
-        name = "Cyanobacteria"
-    ) +
+    scale_color_manual(values = c("N" = "black", "Y" = "aquamarine4")) +
     scale_x_discrete(labels = micro_labels) +
     ylim(0, 50000) +
     labs(
@@ -178,7 +178,7 @@ TPplot_others <- ggplot(
     geom_hline(yintercept = 45570, linetype = "dashed", color = "red", size = 1)
 
 # --------------------------
-#variance explained
+# Variance explained
 # --------------------------
 TPvariance_data <- tibble(
     Factor = c("Cyanobacteria", "Genotype", "Microbiome"),
@@ -202,7 +202,7 @@ TP_variance_plot <- ggplot(
     TPvariance_theme()
 
 # --------------------------
-#combine plots
+# Combine and save
 # --------------------------
 TPmicro_combined_plot <- plot_grid(
     TPplot_microH,
@@ -215,7 +215,7 @@ TPmicro_combined_plot <- plot_grid(
 TPmicro_combined_plot
 
 ggsave(
-    "Expt1_TPmicro_combined_plot.jpg",
+    file.path(plots_folder, "Expt1_TPmicro_combined_plot.jpg"),
     TPmicro_combined_plot,
     width = 8,
     height = 3.25,
