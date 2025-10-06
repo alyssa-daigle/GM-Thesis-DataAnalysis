cb8bb95d497875fd616565a1950f4302771d1dec
alyssa.daigle <alyssa.daigle@unh.edu>
2025-10-06 19:50:16 -0400

    establish Expt2 files/folders

---
+++ Expt2/globals.R
@@ -0,0 +1,24 @@
+invisible(
+  c(
+    "MCMCglmm", #
+    "readxl", #
+    "dplyr", #
+    "tidyr",
+    "purrr", #
+    "stringr",
+    "ggplot2",
+    "cowplot",
+    "forcats",
+    "ggpubr",
+    "tibble",
+    "viridis",
+    "ggpubr",
+    "patchwork"
+  ) |>
+    lapply(function(x) {
+      if (suppressMessages(!require(x, character.only = TRUE))) {
+        install.packages(x)
+        library(x, character.only = TRUE)
+      }
+    })
+)
